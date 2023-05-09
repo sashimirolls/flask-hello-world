97ab23c6550f2918daaac13a2f9df1309224db89
sashimirolls <119821932+sashimirolls@users.noreply.github.com>
2023-05-09 05:30:21 +0000

    y

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from main import convo
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    human_input = request.form['human_input']
+    output = convo.run(human_input)
+    return {'response': output}
+
+if __name__ == '__main__':
+    app.run(debug=True)
